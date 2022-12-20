a9e0e1a77192a515f5bc3c2d066ec519cf059d15
sourabh joshi <sourabh.joshi@aventior.com>
2022-12-20 18:23:00 +0530

    struts project

---
+++ src/main/java/org/apache/struts/loginstruts/model/LoginModel.java
@@ -0,0 +1,68 @@
+package org.apache.struts.loginstruts.model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+public class LoginModel {
+	private String username;
+	private String password;
+	
+	public LoginModel(String username, String password) {
+		setUsername(username);
+		setPassword(password);
+	}
+	
+	public LoginModel() {
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean validateUser() {
+		Connection conn = null;
+
+        try {
+           String URL = "jdbc:mysql://localhost/STRUTS_LOGIN";
+           
+           conn = DriverManager.getConnection(URL, "root", "root");
+           String sql = "SELECT userNAME FROM LOGIN WHERE";
+           
+           sql+=" userNAME = ? AND password = ?";
+           PreparedStatement ps = conn.prepareStatement(sql);
+           
+           ps.setString(1, this.getUsername());
+           ps.setString(2, this.getPassword());
+           
+           ResultSet resultSet = ps.executeQuery();
+
+           while (resultSet.next()) {
+              return true;
+           }
+        } catch (Exception e) {
+        	System.out.println(e);
+           	return false;
+        } finally {
+           if (conn != null) {
+              try {
+                 conn.close();
+              } catch (Exception e) {
+            	  System.out.println(e);
+              }
+           }
+        }
+		return false;
+	}
+}
